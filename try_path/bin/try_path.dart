f41a0d4c2d8b0a0bad8a376ffe27cc89bc48ea7e
SotaAsai <sota.asai.28@gmail.com>
2024-11-26 10:38:56 +0900

    pathパッケージのExampleを確認

--- try_path/bin/try_path.dart
+++ try_path/bin/try_path.dart
@@ -1,5 +1,15 @@
 import 'package:try_path/try_path.dart' as try_path;
+import 'package:path/path.dart' as p;
+
 
 void main(List<String> arguments) {
   print('Hello world: ${try_path.calculate()}!');
+
+  // https://pub.dev/packages/path/example
+  print('Current path style: ${p.style}');
+  print('Current process path: ${p.current}');
+  print('Separators');
+  for (var entry in [p.posix, p.windows, p.url]) {
+    print('  ${entry.style.toString().padRight(7)}: ${entry.separator}');
+  }
 }
