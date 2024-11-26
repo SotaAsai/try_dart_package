633ad862ca294fc44920ec0f1783de81e87118eb
SotaAsai <sota.asai.28@gmail.com>
2024-11-26 10:17:04 +0900

    dart create try_path

---
+++ try_path/bin/try_path.dart
@@ -0,0 +1,5 @@
+import 'package:try_path/try_path.dart' as try_path;
+
+void main(List<String> arguments) {
+  print('Hello world: ${try_path.calculate()}!');
+}
---
+++ try_path/test/try_path_test.dart
@@ -0,0 +1,8 @@
+import 'package:try_path/try_path.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
