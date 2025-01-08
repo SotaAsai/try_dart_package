4cc50ec8314aa6675d68307ef70028e04d1e987c
SotaAsai <sota.asai.28@gmail.com>
2025-01-08 15:11:57 +0900

    dart create try_freezed

---
+++ try_freezed/bin/try_freezed.dart
@@ -0,0 +1,5 @@
+import 'package:try_freezed/try_freezed.dart' as try_freezed;
+
+void main(List<String> arguments) {
+  print('Hello world: ${try_freezed.calculate()}!');
+}
---
+++ try_freezed/test/try_freezed_test.dart
@@ -0,0 +1,8 @@
+import 'package:try_freezed/try_freezed.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
