560d1126ed661e686a5f0121c5b64e989282c611
SotaAsai <sota.asai.28@gmail.com>
2025-01-26 13:23:08 +0900

    freezedはjson_serializableと一緒に使わないと動かないのか？

--- try_freezed/bin/try_freezed.dart
+++ try_freezed/bin/try_freezed.dart
@@ -4,8 +4,10 @@ import 'package:try_freezed/try_freezed.dart' as try_freezed;
 void main(List<String> arguments) {
   print('Hello world: ${try_freezed.calculate()}!');
 
-  // final person = Person(firstName: "taro", lastName: "tanaka", age: 34);
-  // final person2 = person.copyWith;
-
-  // print("${person.toJson()}");
+  final person = Person(firstName: "taro", lastName: "tanaka", age: 34);
+  print(person);
+  final person2 = person.copyWith(firstName: "jiro", age: 31);
+  print(person2);
+  print("${person.toJson()}");
+  print("${person2.toJson()}");
 }
