19c8c828276230147ce502eef283af314836080e
SotaAsai <sota.asai.28@gmail.com>
2025-01-26 12:38:04 +0900

    person.freezed.dartを一旦削除

--- try_freezed/bin/try_freezed.dart
+++ try_freezed/bin/try_freezed.dart
@@ -1,5 +1,11 @@
+import 'package:try_freezed/freezed/person.dart';
 import 'package:try_freezed/try_freezed.dart' as try_freezed;
 
 void main(List<String> arguments) {
   print('Hello world: ${try_freezed.calculate()}!');
+
+  // final person = Person(firstName: "taro", lastName: "tanaka", age: 34);
+  // final person2 = person.copyWith;
+
+  // print("${person.toJson()}");
 }
